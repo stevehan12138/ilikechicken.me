199d41f2b64617c6e9c1d139fefce8da50abf358
ilikechicken <stevehan12138@gmail.com>
2021-11-07 15:55:31 -0500

    more bug fixing

--- ilikechicken/views.py
+++ ilikechicken/views.py
@@ -1,4 +1,5 @@
 import os
+import hmac
 import hashlib
 import json
 from django.views.decorators.http import require_POST
@@ -9,15 +10,18 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 @require_POST
 def postreceive(request):
-    if 'X_GITHUB_EVENT' not in request.headers:
+    if 'X_GITHUB_EVENT' not in request.headers or 'X_Hub_Signature_256' not in request.headers:
         return HttpResponse(status=400)
     event = request.headers['X_GITHUB_EVENT']
     if event == 'ping':
         return HttpResponse('pong')
     elif event == 'push':
         json_data = json.loads(request.body)
-        if hashlib.sha256().update(json_data['hook']['config']['secret']).hexdigest() == constants.POST_RECEIVE_SECRET:
+        if json_data['ref'] == 'refs/heads/main':
+            signature = hmac.new(constants.POST_RECEIVE_SECRET , request.body, hashlib.sha256).hexdigest()
+            expected = request.headers['X_Hub_Signature_256']
+            if not hmac.compare_digest(signature, expected):
+                return HttpResponse(status=403)
             os.system('bash ./deploy.sh')
-        else:
-            return HttpResponse('Invalid secret key')
+            return HttpResponse('success')
     return HttpResponse(status=204)
