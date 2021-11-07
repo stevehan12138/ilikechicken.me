49b432a08d8e5444816c57389ec2d048fd538ae1
ilikechicken <stevehan12138@gmail.com>
2021-11-07 16:09:42 -0500

    moremore bug fixes

--- ilikechicken/views.py
+++ ilikechicken/views.py
@@ -6,22 +6,23 @@ from django.views.decorators.http import require_POST
 from django.http import HttpResponse
 from ilikechicken import constants
 from django.views.decorators.csrf import csrf_exempt
+from django.utils.encoding import force_bytes
 
 @csrf_exempt
 @require_POST
 def postreceive(request):
     if 'X_GITHUB_EVENT' not in request.headers or 'X_Hub_Signature_256' not in request.headers:
         return HttpResponse(status=400)
+    json_data = json.loads(request.body)
+    if json_data['ref'] == 'refs/heads/main':
+        signature = hmac.new(force_bytes(constants.POST_RECEIVE_SECRET), msg=force_bytes(request.body), digestmod=hashlib.sha256)
+        expected = request.headers['X_Hub_Signature_256'].split('=')[1]
+        if not hmac.compare_digest(force_bytes(signature.hexdigest()), force_bytes(expected)):
+            return HttpResponse(status=403)
     event = request.headers['X_GITHUB_EVENT']
     if event == 'ping':
         return HttpResponse('pong')
     elif event == 'push':
-        json_data = json.loads(request.body)
-        if json_data['ref'] == 'refs/heads/main':
-            signature = hmac.new(constants.POST_RECEIVE_SECRET , request.body, hashlib.sha256).hexdigest()
-            expected = request.headers['X_Hub_Signature_256']
-            if not hmac.compare_digest(signature, expected):
-                return HttpResponse(status=403)
-            os.system('bash ./deploy.sh')
-            return HttpResponse('success')
+        os.system('bash ./deploy.sh')
+        return HttpResponse('success')
     return HttpResponse(status=204)
