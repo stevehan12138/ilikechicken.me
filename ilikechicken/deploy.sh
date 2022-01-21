d80d9c3bd8aedf732fa97af8b1b2f18b51259fc0
Steve <stevehan12138@gmail.com>
2022-01-20 23:03:21 -0500

    postreceive update

--- ilikechicken/deploy.sh
+++ ilikechicken/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-workon website
-cd ~/ilikechicken
+source ~/apps/website/env/bin/activate
+cd ~/apps/website/ilikechicken.me
 git pull
+python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
+touch ~/apps/website/ilikechicken.me/wsgi.py
 echo done
